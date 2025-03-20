7d4420eeb00099ef3858b76d2fc1f280df386f16
Cristian Rinaldi <cristianrinaldi0@gmail.com>
2025-03-20 13:52:38 +0100

    aggiunto js

---
+++ js-cris.js
@@ -0,0 +1,13 @@
+
+setTimeout(()=>{
+    const img= document.querySelector(".main-img")
+    img.src= "img/img-adidas.avif"
+   
+},4000)
+
+setTimeout(()=>{
+    const img= document.querySelector(".main-img")
+    img.src= "img/Scarpe_Campus_00s_Blu_JR8163_01_00_standard.avif"
+   img.style.height = "650px"
+ 
+},8000)
