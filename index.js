03c4101ab9722eb037531410c8678d369962e904
Alfonso Lambiase <lambiase1199@gmail.com>
2025-03-20 17:39:09 +0100

    add modified

--- index.js
+++ index.js
@@ -2,14 +2,23 @@
 
 const target = document.getElementById("target"); 
 const tooltip = document.getElementById("tooltip");
-
+const target2 = document.getElementById("target2");
+const tooltip2 = document.getElementById("tooltip2");
 
 target.addEventListener("mouseover", () => {
-  tooltip.style.visibility = "visible";
+  tooltip.style.display = "block";
 });
 
 // Nasconde il tooltip
 target.addEventListener("mouseleave", () => {
-  tooltip.style.visibility = "hidden";
+  tooltip.style.display = "none";
 });
 
+target2.addEventListener("mouseover", () => {
+  tooltip2.style.display = "block";
+});
+
+// Nasconde il tooltip
+target2.addEventListener("mouseleave", () => {
+  tooltip2.style.display = "none";
+});
