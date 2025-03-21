2f81e5633a8b0b09631f673a21845420d9634a55
Cristian Rinaldi <cristianrinaldi0@gmail.com>
2025-03-21 01:14:58 +0100

    aggiunto j.s.

--- js-cris.js
+++ js-cris.js
@@ -5,9 +5,17 @@ setTimeout(()=>{
    
 },4000)
 
+
+setTimeout(()=>{
+    const img= document.querySelector(".main-img")
+    img.src= src="https://brand.assets.adidas.com/video/upload/f_auto,q_auto/if_w_gt_1920,w_1920/global_main_pack_1_pure_victory_football_ss25_launch_hp_banner_hero_d_05b272a381.jpg"
+   
+ 
+},8000)
+
 setTimeout(()=>{
     const img= document.querySelector(".main-img")
     img.src= "img/Scarpe_Campus_00s_Blu_JR8163_01_00_standard.avif"
    img.style.height = "650px"
  
-},8000)
+},12000)
